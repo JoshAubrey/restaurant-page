a3b71ca47c01da7a8db156c677f655a92058af18
JoshAubrey <joshaubrey7@gmail.com>
2019-09-24 16:04:41 -0500

    Create/convert remaining html to js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,28 @@ import foodMenu from "./foodMenu.js"
 import location from "./location.js"
 
 header()
+about()
+
+function clearNavSelection () {
+    document.querySelectorAll('.menu-item').forEach(menuItem => 
+        menuItem.classList.remove('menu-item-selected')
+        )
+}
+
+document.querySelectorAll(".menu-item").forEach( button =>
+        button.addEventListener("click", function(e) {
+          clearNavSelection()
+          document.getElementById('content').removeChild(document.querySelector('.inner-content'))
+          switch(e.target.id){
+              case 'about':
+                  about()
+              break
+              case 'menu':
+                  foodMenu()
+              break
+              case 'location':
+                  location()
+              break
+          }
+        })
+  )
